0be0534a4de4d428086d6fec25df259968d19f58
killuaxhunterx <arthurnegaodebsb@gmail.com>
2025-11-24 13:59:20 -0300

    feat: adiciona propriedades lastCardGiven e scoreHand

--- src/model/DealerEntity.ts
+++ src/model/DealerEntity.ts
@@ -3,6 +3,8 @@ import { Cards } from "./CardsEnum.js";
 export class DealerEntity {
 
     private dealersHand: Cards[] = [];
+    private lastCardGiven: number = 0;
+    private scoreHand: number = 0;
 
     getDealersHand(): Cards[] {
         return this.dealersHand;
@@ -10,6 +12,22 @@ export class DealerEntity {
 
     setDealersHand(cards: Cards[]): void {
         this.dealersHand = cards;
+    }
+
+    getLastCardGiven(): number {
+        return this.lastCardGiven;
+    }
+
+    setLastCardGiven(lastCardGiven: number): void {
+        this.lastCardGiven = lastCardGiven;
+    }
+
+    getScoreHand(): number {
+        return this.scoreHand;
+    }
+
+    setScoreHand(score: number): void {
+        this.scoreHand = score;
     }
 
 }
