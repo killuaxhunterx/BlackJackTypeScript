36bc8677b2199f03fd27359ab41696fe76b7fdd9
killuaxhunterx <arthurnegaodebsb@gmail.com>
2025-11-24 14:04:56 -0300

    feat: tipos adicionados para ajudar na legibilidade do código

---
+++ src/model/WinnerType.ts
@@ -0,0 +1,5 @@
+import type { DealerEntity } from "./DealerEntity.js";
+import type { PlayerEntity } from "./PlayerEntity.js";
+
+export type Draw = "Draw";
+export type Winner = PlayerEntity | DealerEntity;
